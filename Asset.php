ee3050e290f731bef02fd7957e82d3dc754d942a
Semenov <semenov@skeeks.com>
2014-10-21 19:59:38 +0400

    Start wiget develop

---
+++ Asset.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Asset
+ *
+ * @author Semenov Alexander <semenov@skeeks.com>
+ * @link http://skeeks.com/
+ * @copyright 2010-2014 SkeekS (Sx)
+ * @date 21.10.2014
+ * @since 1.0.0
+ */
+namespace skeeks\widget\simpleajaxloader;
+
+use yii\web\AssetBundle;
+
+/**
+ * Class Core
+ * @package skeeks\widget\simpleajaxloader
+ */
+class Asset extends AssetBundle
+{
+    public $sourcePath = '@vendor/skeeks/yii2-widget-simpleajaxloader/assets';
+    public $css = [];
+    public $js = [
+        'SimpleAjaxUploader.min.js'
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+    ];
+}
